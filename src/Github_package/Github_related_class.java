c9baaaa0456975484e547084024c0cda5a18d2da
cnspraveen <cns.praveen@gmail.com>
2016-07-10 14:17:44 +0530

    changes done

--- src/Github_package/Github_related_class.java
+++ src/Github_package/Github_related_class.java
@@ -5,6 +5,6 @@ public class Github_related_class
  public static void main(String args[])
  {
 	 System.out.println("coded from eclipse");
-	 System.out.println("coded from github")
+	 System.out.println("coded from github");
  }
 }
